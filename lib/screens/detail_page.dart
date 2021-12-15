262766ca386fb76a5b0bd99e98be5604643e57c8
Shokhrukh <shokhrukh.rakhmatov1234@gmail.com>
2021-12-15 20:01:45 +0500

    smt

---
+++ lib/screens/detail_page.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:daryo_uz/models/news_list.dart';
+
+class DetailPage{
+
+
+}
--- lib/screens/news.dart
+++ lib/screens/news.dart
@@ -10,47 +10,50 @@ class News extends StatelessWidget {
       child:ListView.builder(
           itemCount: NewsWidget.newlist.length,
           itemBuilder: (BuildContext context, int index){
-            return Card(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        TextButton(
-                            onPressed: (){},
-                            child: Text(NewsWidget.newlist[index].typeNews)),
-                        Row(
-                          children: [
-                            Text(NewsWidget.newlist[index].infoNews),
-                            SizedBox(width: 8,),
-                            Icon(Icons.remove_red_eye_outlined, color: Colors.blue,),
-                            SizedBox(width: 8,),
-                            Text(NewsWidget.newlist[index].viewCount.toString(),
-                              style: TextStyle( color: Colors.blue) ,),
-                          ],
-                        )
-                             ],
-                    ),
-                  ),
-                  Row(
+            return GestureDetector(
+              onTap: ()=>{},
+              child: Card(
+                child: Column(
                   children: [
-                    Expanded(
-                      flex: 2,
-                      child: Image.asset(NewsWidget.newlist[index].imageUrl),
-                    ),
-                    Expanded(
-                      flex: 3,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(NewsWidget.newlist[index].textNews),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          TextButton(
+                              onPressed: (){},
+                              child: Text(NewsWidget.newlist[index].typeNews)),
+                          Row(
+                            children: [
+                              Text(NewsWidget.newlist[index].infoNews),
+                              SizedBox(width: 8,),
+                              Icon(Icons.remove_red_eye_outlined, color: Colors.blue,),
+                              SizedBox(width: 8,),
+                              Text(NewsWidget.newlist[index].viewCount.toString(),
+                                style: TextStyle( color: Colors.blue) ,),
+                            ],
+                          )
+                               ],
                       ),
                     ),
+                    Row(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Image.asset(NewsWidget.newlist[index].imageUrl),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(NewsWidget.newlist[index].textNews),
+                        ),
+                      ),
+                    ],
+                    ),
                   ],
-                  ),
-                ],
-              )
+                )
+              ),
             );
 
           }),
